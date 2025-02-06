ac46b9746828edb07c0ab2d18fe518ea7b971e6e
iagorivasalonso <72391242+iagorivasalonso@users.noreply.github.com>
2025-02-06 12:15:15 +0100

    Reestructure_main

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:crud_factories/generated/l10n.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-
 
   await windowManager.ensureInitialized();
 
@@ -17,71 +15,28 @@ void main() async {
     center: true,
     title: "Mi aplicación",
   );
+
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
   });
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-
-  final Locale _localeSpanish = const Locale('es');
-
   Widget build(BuildContext context) {
-
     return FluentApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp1(),
-    );
-  }
-}
-
-class MyApp1 extends StatefulWidget {
-  @override
-  State<MyApp1> createState() => _MyApp1State();
-}
-
-class _MyApp1State extends State<MyApp1> with WindowListener {
-
-  @override
-  void initState() {
-    super.initState();
-    windowManager.addListener(this);
-    _init();
-  }
-
-  @override
-  void dispose() {
-    windowManager.removeListener(this);
-  }
-
-  void _init() async {
-    await windowManager.setPreventClose(true);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return const FluentApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
+      localizationsDelegates: const [
         S.delegate,
-        GlobalMaterialLocalizations.delegate,
+        ...GlobalMaterialLocalizations.delegates,
         GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
+        GlobalCupertinoLocalizations.delegate,
       ],
+      supportedLocales: S.delegate.supportedLocales,
       home: App(),
     );
   }
-  @override
-  void onWindowClose() async {
-
-    closeAlert(context);
-
-  }
-
-
 }
