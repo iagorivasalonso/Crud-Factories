c2f79fa93d4c3273b6a434e68d9bff4fa5548e31
iagorivasalonso <72391242+iagorivasalonso@users.noreply.github.com>
2025-03-12 15:58:47 +0100

    add_enum_state

--- lib/Backend/SQL/modifyLines.dart
+++ lib/Backend/SQL/modifyLines.dart
@@ -1,23 +1,22 @@
-import 'package:crud_factories/Backend/SQL/importLines.dart';
 import 'package:crud_factories/Backend/data.dart';
+import 'package:crud_factories/Functions/manageState.dart';
 import 'package:crud_factories/Objects/LineSend.dart';
-import 'package:crud_factories/Objects/Mail.dart';
 
 Future<void> sqlModifyLines(List<LineSend> lineSelected) async {
 
   try{
-
+/*
       for (int i = 0; i <lineSelected.length; i++)
       {
         String id = lineSelected[i].id;
         String date = lineSelected[i].date;
         String factory = lineSelected[i].factory;
         String observations = lineSelected[i].observations;
-        String state = lineSelected[i].state;
+        String state = parseState(lineSelected[i].state.toString()).name;
 
         var result = await conn.query('update linesends set date=?,factory=?, observations=?,  state=? where id=?', [date, factory, observations,state, id]);
       }
-
+*/
   } catch(SQLExeption){
 
   }
