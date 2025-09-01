a4325fec40d0a9b0dfb8be1d968175ac9e881537
iagorivasalonso <72391242+iagorivasalonso@users.noreply.github.com>
2025-09-01 12:38:37 +0200

    add controllerMail

---
+++ lib/Backend/Global/controllers/Mail.dart
@@ -0,0 +1,14 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class MailControllers {
+
+  final TextEditingController mail;
+  final TextEditingController password;
+  final TextEditingController passwordVerify;
+
+  MailControllers({
+    required this.mail,
+    required this.password,
+    required this.passwordVerify,
+  });
+}
