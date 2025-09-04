ef7e5eb86d83eb2b649811a21b9a36d3bebccf53
iagorivasalonso <72391242+iagorivasalonso@users.noreply.github.com>
2025-09-04 10:30:16 +0200

    create_controller

---
+++ lib/Backend/Global/controllers/Factory.dart
@@ -0,0 +1,41 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class factoryController {
+
+  final TextEditingController name;
+  final TextEditingController highDate;
+  final TextEditingController sector;
+  final TextEditingController telephone1;
+  final TextEditingController telephone2;
+  final TextEditingController mail;
+  final TextEditingController web;
+  final TextEditingController address;
+  final TextEditingController city;
+  final TextEditingController postalCode;
+  final TextEditingController province;
+  final List<TextEditingController> contacts;
+  final TextEditingController employee;
+  final TextEditingController employeeNew;
+
+  factoryController({
+  required this.name,
+  required this.highDate,
+  required this.sector,
+  required this.telephone1,
+  required this.telephone2,
+  required this.mail,
+  required this.web,
+  required this.address,
+  required this.city,
+  required this.postalCode,
+  required this.province,
+  required this.contacts,
+  required this.employee,
+  required this.employeeNew,
+  });
+
+  }
+
+
+
+
