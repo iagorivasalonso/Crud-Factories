033ebb6e79217f58acc7556f158f3bd8c6573ebe
Iago Rivas <iagorivas@gmail.com>
2025-09-12 16:35:46 +0200

    modif_controler

--- lib/Backend/Global/controllers/Mail.dart
+++ lib/Backend/Global/controllers/Mail.dart
@@ -1,14 +1,28 @@
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 
 class MailController {
 
   final TextEditingController mail;
   final TextEditingController password;
-  final TextEditingController passwordVerify;
+  final TextEditingController? passwordVerify;
+  final TextEditingController? mailTo;
+  final TextEditingController? subject;
+  final TextEditingController? message;
+  final List<File> attachments;
 
   MailController({
     required this.mail,
     required this.password,
-    required this.passwordVerify,
-  });
+    this.passwordVerify,
+    this.mailTo,
+    this.subject,
+    this.message,
+    List<File>? attachments,
+
+
+  }) :attachments = attachments ?? [];
+
+  bool get hasAttachments => attachments.isNotEmpty;
 }
