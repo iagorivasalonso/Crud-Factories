3d530b4be72a5c2a2acdcff7165f6fbb6373621a
Iago Rivas <iagorivas@gmail.com>
2026-01-29 13:55:07 +0100

    quitar_librari_sobrant

--- lib/Backend/CSV/ImportGeneral/importCsvSafe.dart
+++ lib/Backend/CSV/ImportGeneral/importCsvSafe.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:crud_factories/Backend/CSV/ImportGeneral/AppFile.dart';
 import 'package:fluent_ui/fluent_ui.dart' show BuildContext;
 import 'package:flutter/services.dart';
