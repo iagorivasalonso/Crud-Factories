fb3877b83f8012bc8be36edc4e684f4ff8470c19
Iago Rivas <iagorivas@gmail.com>
2026-02-27 11:24:27 +0100

    aptar_api_controllers_global

--- lib/Backend/connectors_API/connectApi.dart
+++ lib/Backend/connectors_API/connectApi.dart
@@ -1,17 +1,18 @@
-import 'package:crud_factories/Backend/Global/controllers/Conection.dart';
 
-Future<Uri> connectApi(connectionControler controllers, String nameTable) async {
+import '../Global/list.dart';
+
+Future<Uri> connectApi(String nameTable) async {
 
   return Uri(
     scheme: 'http',
     host: 'localhost',
     port: 3000,
-    path: '${controllers.namebd.text}/$nameTable',
+    path: '${controlerConex.namebd.text}/$nameTable',
     queryParameters: {
-      'host': controllers.hostbd.text,
-      'port': controllers.portbd.text,
-      'user': controllers.userbd.text,
-      'password': controllers.passbd.text,
+      'host': controlerConex.hostbd.text,
+      'port': controlerConex.portbd.text,
+      'user': controlerConex.userbd.text,
+      'password': controlerConex.passbd.text,
     },
   );
 }
