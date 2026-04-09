86cf0ef9fba6445d090d3809f030d0db9ac428a9
Iago Rivas <iagorivas@gmail.com>
2026-04-09 12:17:13 +0200

    igualar formato a otros elementos

--- lib/Widgets/listElements.dart
+++ lib/Widgets/listElements.dart
@@ -1,5 +1,6 @@
 import 'package:crud_factories/Objects/Empleoye.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' hide Colors;
 
 class ContactList extends StatefulWidget {
 
@@ -16,41 +17,53 @@ class _ContactListState extends State<ContactList> {
 
   @override
   Widget build(BuildContext context) {
+
+    final borderColor = Theme.of(context).colorScheme.outline;
+
     return Padding(
-      padding: const EdgeInsets.only(left: 35.0, top: 20.0, bottom: 30.0, right: 90.0),
+      padding: const EdgeInsets.only(
+        left: 55.0,
+        top: 20.0,
+        bottom: 30.0,
+        right: 70.0,
+      ),
       child: Container(
+        width: 340,
+        height: 170,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
+          borderRadius: BorderRadius.circular(4), // estilo TextField
           border: Border.all(
-            color:const Color(0xFFB0B0B0),
+            color: borderColor, // mismo color que inputs
             width: 1.0,
           ),
         ),
-        width: 340,
-        height: 170,
-        child: ListView.builder(
-          itemCount: widget.contacsCurrent.length,
-          itemBuilder: (BuildContext context, int index) {
-            final employee = widget.contacsCurrent[index];
-            final isSelected = index == contactSelect;
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  contactSelect = index;
-                });
-              },
-              child: Container(
-                color: isSelected ? Colors.blue : Colors.white,
-                padding: const EdgeInsets.only(top: 3.0, left: 10.0),
-                child: Text(
-                  employee.name,
-                  style: TextStyle(
-                    color: isSelected ? Colors.white : Colors.black,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(4),
+          child: ListView.builder(
+            itemCount: widget.contacsCurrent.length,
+            itemBuilder: (BuildContext context, int index) {
+              final employee = widget.contacsCurrent[index];
+              final isSelected = index == contactSelect;
+
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    contactSelect = index;
+                  });
+                },
+                child: Container(
+                  color: isSelected ? Colors.blue : Colors.white,
+                  padding: const EdgeInsets.only(top: 3.0, left: 10.0),
+                  child: Text(
+                    employee.name,
+                    style: TextStyle(
+                      color: isSelected ? Colors.white : Colors.black,
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
