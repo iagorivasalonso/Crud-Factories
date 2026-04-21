5bf5fd16d270acf378aac55e2c3197fa9ff2f762
Iago Rivas <iagorivas@gmail.com>
2026-04-21 13:16:48 +0200

    exportacion archivos a zip

--- lib/Backend/CSV/exportRoutes.dart
+++ lib/Backend/CSV/exportRoutes.dart
@@ -1,38 +1,32 @@
+import 'package:crud_factories/Backend/Global/files.dart' show fRoutes;
+import 'package:crud_factories/Objects/RouteCSV.dart' show RouteCSV;
 import 'package:csv/csv.dart';
 
-import '../../Objects/RouteCSV.dart';
-import '../Global/files.dart';
-import 'Export_general/export_csv.dart';
 
-
+import 'Export_general/export_csv_io.dart';
 
 Future<bool> csvExportatorRoutes(List<RouteCSV> routes) async {
+  final rows = <List<dynamic>>[];
 
-  List<dynamic> associateList = [
+  rows.add([
+    'id',
+    'name',
+    'route',
+  ]);
 
-      for (int i = 0; i < routes.length; i++)
-      {
-        "id": routes[i].id,
-        "name": routes[i].name,
-        "route": routes[i].route
-      },
-  ];
-
-  List<List<dynamic>> rows = [];
-
-  for (int i = 0; i < associateList.length; i++)
-  {
-    List<dynamic> row = [];
-
-    row.add(associateList[i]["id"]);
-    row.add(associateList[i]["name"]);
-    row.add(associateList[i]["route"]);
-    rows.add(row);
+  for (final r in routes) {
+    rows.add([
+      r.id,
+      r.name,
+      r.route,
+    ]);
   }
-  final filePath = fRoutes.path;
 
- String csv = const ListToCsvConverter(fieldDelimiter: ';').convert(rows);
-  final success  = await exportCsv(csv, file: filePath);
+  final csv = const ListToCsvConverter(
+    fieldDelimiter: ';',
+  ).convert(rows);
+
+  final success = await exportCsv(csv, file: fRoutes.path);
 
   return !success;
 }
