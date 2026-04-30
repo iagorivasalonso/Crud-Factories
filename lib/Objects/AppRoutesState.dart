9f5fa0ea2aba643c40512737465be165ce3b66d4
Iago Rivas <iagorivas@gmail.com>
2026-04-30 20:42:40 +0200

    class_router_file

---
+++ lib/Objects/AppRoutesState.dart
@@ -0,0 +1,21 @@
+class RouteFiles {
+  final String routes;
+  final String connections;
+  final String server;
+  final String sectors;
+  final String factories;
+  final String employees;
+  final String lines;
+  final String mails;
+
+  RouteFiles({
+    required this.routes,
+    required this.connections,
+    required this.server,
+    required this.sectors,
+    required this.factories,
+    required this.employees,
+    required this.lines,
+    required this.mails,
+  });
+}
