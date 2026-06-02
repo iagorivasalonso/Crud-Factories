f9ed5e99a8a250f0922f99ba8bad7441af3573be
Iago Rivas <iagorivas@gmail.com>
2026-06-02 17:42:46 +0200

    crar_objecto_aparte

---
+++ lib/Objects/ApiConfig.dart
@@ -0,0 +1,15 @@
+class ApiConfig {
+  final String host;
+  final String port;
+  final String database;
+  final String user;
+  final String password;
+
+  ApiConfig({
+    required this.host,
+    required this.port,
+    required this.database,
+    required this.user,
+    required this.password,
+  });
+}
