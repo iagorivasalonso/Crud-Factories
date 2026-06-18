278981f0e943c5ff3b675d76690f437e43045100
Iago Rivas <iagorivas@gmail.com>
2026-06-18 18:02:20 +0200

    cambio_firma

--- lib/Backend/Feature/Connection/Sesion/api_connection_sesion_service.dart
+++ lib/Backend/Feature/Connection/Sesion/api_connection_sesion_service.dart
@@ -1,39 +1,43 @@
 
+import 'package:crud_factories/Backend/Feature/Connection/Controller/ConnectionController.dart' show DisconnectResponse;
+import 'package:crud_factories/Backend/Feature/Connection/ExecuteQuery/IexecuteQuery.dart' show Iexecutequery;
+import 'package:crud_factories/Backend/Feature/Connection/ExecuteQuery/apiExecuteQuery.dart' show ApiExecuteQuery;
 import 'package:crud_factories/Backend/Feature/Connection/Sesion/IConnection_sesion_service.dart' show IConnectionSesionService;
 import 'package:crud_factories/Backend/connectors_API/DbApi.dart' show DbApi;
 import 'package:crud_factories/Backend/connectors_API/Models/Api_response.dart';
 import 'package:crud_factories/Objects/Conection.dart';
-import 'package:mysql1/mysql1.dart' show MySqlConnection;
 
-class apiConnectionService implements IConnectionSesionService{
+
+class apiConnectionSesionService implements IConnectionSesionService{
+
+  late final Iexecutequery _executeQuery = ApiExecuteQuery();
 
   @override
-  Future<MySqlConnection> connect(Conection c) async {
+  Future<void> connect(Conection c) async {
 
-      final res = await DbApi.actionApi(
-          'connect',
-          c
-      );
+    final res = await DbApi.actionApi('connect', c);
 
-      final response = ApiResponse.fromJson(res);
+    final response = ApiResponse.fromJson(res);
 
-
-      if (!response.ok) {
-        throw Exception(response.message);
-      }
-
-      return response.data;
+    if (!response.ok) {
+      throw Exception(response.message);
+    }
   }
 
   @override
-  Future<bool> disconnect(Conection c) async {
+  Iexecutequery get executeQuery => _executeQuery;
 
-    await DbApi.actionApi(
-      'disconnect',
-      null,
+  @override
+  Future<DisconnectResponse> disconnect() async {
+
+    final res = await DbApi.actionApi('disconnect', null);
+
+    final response = ApiResponse.fromJson(res);
+
+
+    return DisconnectResponse(
+      ok: response.ok,
+      message: response.message,
     );
-
-    return true;
   }
-
 }
