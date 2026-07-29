5d1f4901dc167a5fef74a7eb41e704abbb838f63
Iago Rivas <iagorivas@gmail.com>
2026-07-29 13:09:19 +0200

    modify_mail

--- lib/Objects/Mail.dart
+++ lib/Objects/Mail.dart
@@ -5,51 +5,40 @@ import 'package:crud_factories/Objects/BaseEntity.dart';
 class Mail extends BaseEntity {
 
    String id;
-   String address;
-   String company;
+   String mail;
+   String host;
+   String port;
+   bool secure;
    String password;
 
    Mail({
     required this.id,
-    required this.address,
-    required this.company,
+    required this.mail,
+    required this.host,
+    required this.port,
+    required this.secure,
     required this.password
    });
 }
 
-class MessageMail extends BaseEntity {
+class MailResult {
+  final bool success;
+  final List<String> sent;
+  final List<MailFailure> failed;
 
-  final String host;
-  final int port;
-  final bool secure;
-  final String username;
-  final String password;
-  final List<dynamic> mails;
-  final String subject;
-  final String message;
-  final List<Map<String, dynamic>>? attachments;
-
-  MessageMail ({
-    required this.host,
-    required this.port,
-    required this.secure,
-    required this.username,
-    required this.password,
-    required this.mails,
-    required this.subject,
-    required this.message,
-    this.attachments
+  MailResult({
+    required this.success,
+    required this.sent,
+    required this.failed,
   });
+}
 
-  Map<String, dynamic> toJson() => {
-    'host': host,
-    'port': port,
-    'secure': secure,
-    'username': username,
-    'password': password,
-    'mail': mails,
-    'subject': subject,
-    'message': message,
-    'attachments': attachments
-  };
+class MailFailure {
+  final String mail;
+  final String error;
+
+  const MailFailure({
+    required this.mail,
+    required this.error,
+  });
 }
