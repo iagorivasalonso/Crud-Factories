0b2a1570a38d1888ba72736747851fe6c74d39b2
Iago Rivas <iagorivas@gmail.com>
2026-08-19 13:01:36 +0200

    corregir la clase, y permitir nulos donde fue necesario

--- lib/Backend/SQL/manageSQl.dart
+++ lib/Backend/SQL/manageSQl.dart
@@ -1,47 +1,52 @@
 import 'package:crud_factories/Backend/Global/variables.dart';
+import 'package:mysql1/src/single_connection.dart';
 
 
 class actionsBD {
+
   static Future<bool> createDB(String nameBD, conn) async {
 
     try {
+
       await conn.query('CREATE DATABASE $nameBD');
       await conn.query('USE `$nameBD`');
+
       return true;
-    }catch(SQLException){
+
+    }catch(_){
       return false;
     }
+
   }
 
-  static Future<bool> createTables() async {
+  static Future<bool> createTables(MySqlConnection conn) async {
 
-    bool err=false;
-    print(executeQuery);
+
     try {
-      await executeQuery.query('CREATE TABLE IF NOT EXISTS sectors '
+      await conn.query('CREATE TABLE IF NOT EXISTS sectors '
           '(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,'
           ' sector varchar(50) NOT NULL)'
       );
 
-      await executeQuery.query('CREATE TABLE IF NOT EXISTS factories '
+      await conn.query('CREATE TABLE IF NOT EXISTS factories '
           '(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,'
           ' name varchar(255) NOT NULL, '
-          ' highDate varchar(12) NOT NULL,'
-          ' sector int(11) NOT NULL,'
-          ' telephone1 varchar(9) NOT NULL,'
-          ' telephone2 varchar(9) NOT NULL,'
-          ' mail varchar(50) NOT NULL,'
-          ' web varchar(100) NOT NULL,'
-          ' address varchar(255) NOT NULL, '
-          ' number varchar(4) NOT NULL,'
-          ' apartament varchar(10) NOT NULL,'
-          ' city varchar(10) NOT NULL, '
-          ' province varchar(10) NOT NULL, '
-          ' postalcode varchar(5) NOT NULL, '
+          ' highDate varchar(12),'
+          ' sector int(11),'
+          ' telephone1 varchar(9),'
+          ' telephone2 varchar(9),'
+          ' mail varchar(50),'
+          ' web varchar(100),'
+          ' address varchar(255), '
+          ' number varchar(4),'
+          ' apartment varchar(10),'
+          ' city varchar(10), '
+          ' province varchar(10), '
+          ' postcode varchar(5), '
           ' FOREIGN KEY fk_sectors(sector) REFERENCES sectors(id))'
       );
 
-      await executeQuery.query('CREATE TABLE IF NOT EXISTS employees '
+      await conn.query('CREATE TABLE IF NOT EXISTS employees '
           '(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,'
           ' name varchar(50) NOT NULL,'
           ' idFactory int(11) NOT NULL,'
@@ -49,63 +54,81 @@ class actionsBD {
           ' ON DELETE CASCADE)'
       );
 
-      await executeQuery.query('CREATE TABLE IF NOT EXISTS lineSends '
+      await conn.query('CREATE TABLE IF NOT EXISTS lineSends '
           '(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,'
           ' date varchar(12) NOT NULL, '
           ' factory varchar(255) NOT NULL,'
-          ' observations varchar(100) NOT NULL,'
-          ' state varchar(20) NOT NULL)'
+          ' observations varchar(100),'
+          ' state varchar(20))'
       );
 
-      await executeQuery.query('CREATE TABLE IF NOT EXISTS mails '
-          '(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,'
-          ' company varchar(20) NOT NULL, '
-          ' email varchar(50) NOT NULL,'
-          ' password varchar(100) NOT NULL)'
+      await conn.query('CREATE TABLE IF NOT EXISTS mails'
+         '(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+          'mail VARCHAR(100) NOT NULL,'
+          'host VARCHAR(100) NOT NULL,'
+          'port INT NOT NULL,'
+          'secure BOOLEAN NOT NULL,'
+          'password VARCHAR(255) NOT NULL)'
       );
-
-    }catch(SQLException){
-      err = true;
+      return true;
+    }catch(e){
+      print(e);
+      return false;
     }
 
-    return err;
+
   }
 
-  static Future<bool> deleteDB(String nameBD) async {
-
-    bool err= false;
+  static Future<bool> deleteDB(String nameBD, MySqlConnection conn) async {
 
     try {
 
-      await executeQuery.query('DROP DATABASE $nameBD');
-
-    }catch(SQLException){
-      err=true;
+      await conn.query('DROP DATABASE $nameBD');
+      return true;
+    }catch(_){
+      return false;
     }
 
-    return err;
+
   }
 
-  static Future<bool> editDB(String nameBD, String nameBDnew) async {
+  static Future<bool> editDB(String nameBD, String nameBDnew, MySqlConnection conn) async {
 
-    bool err=false;
 
     try {
-      var results = await executeQuery.query('SHOW TABLES');
-      await executeQuery.query('CREATE DATABASE $nameBDnew');
+
+      await conn.query(
+        'USE `$nameBD`',
+      );
+
+      var results =
+      await conn.query(
+          'SHOW TABLES'
+      );
+
+      await conn.query(
+          'CREATE DATABASE $nameBDnew'
+      );
+
+            
       for (final row in results) {
-        final nameTable = row[0].toString();
-        await executeQuery.query(
-            'RENAME TABLE $nameBD.$nameTable TO $nameBDnew.$nameTable');
+
+            final nameTable = row[0].toString();
+
+            await conn.query(
+                'RENAME TABLE '
+                    '$nameBD.$nameTable '
+                    'TO '
+                    '$nameBDnew.$nameTable');
       }
 
-      await executeQuery.query('DROP DATABASE $nameBD');
-
+      await conn.query('DROP DATABASE $nameBD');
+      return true;
     }catch(e){
-      err = true;
+      print(e);
+      return true;
     } finally {
-      await executeQuery.close();
+      await conn.close();
     }
-    return err;
   }
 }
