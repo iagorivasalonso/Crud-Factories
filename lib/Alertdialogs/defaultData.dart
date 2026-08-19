52bbf9df1e1cba8e55ac42fd36b7f8734d6ce124
Iago Rivas <iagorivas@gmail.com>
2026-08-19 12:49:38 +0200

    refactor_metodo_segun_platform

--- lib/Alertdialogs/defaultData.dart
+++ lib/Alertdialogs/defaultData.dart
@@ -1,77 +1,75 @@
 import 'package:crud_factories/Widgets/headAlertDialog.dart';
 import 'package:crud_factories/generated/l10n.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
-Future<bool> defaultData (BuildContext  context) async {
+enum FuenteData {
+     defaultData,
+     newData
+}
+Future<FuenteData> defaultData (BuildContext  context) async {
 
-  bool? dataDefault  = await   showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context, ) {
+          return await   showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (BuildContext context, ) {
 
-        String title = kIsWeb
-            ? S.of(context).select_how_continue
-            : S.of(context).data_not_found;
+                  String title = S.of(context).select_how_continue;
 
-        return StatefulBuilder(
-          builder: (BuildContext context0, void Function(void Function()) setState) => Dialog(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-            child: SizedBox(
-              width: 500,
-              height: 175,
-              child: Column(
-                children: [
-                  headDialog(title: title),
-                  Expanded(
-                    child: Padding(
-                      padding:  EdgeInsets.only(left: 25,top: 25, bottom: 15),
-                      child: Text(S.of(context).no_data_available,
+                  return StatefulBuilder(
+                    builder: (BuildContext context0, void Function(void Function()) setState) => Dialog(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+                      child: SizedBox(
+                        width: 500,
+                        height: 175,
+                        child: Column(
+                          children: [
+                            headDialog(title: title),
+                            Expanded(
+                              child: Padding(
+                                padding:  EdgeInsets.only(left: 25,top: 25, bottom: 15),
+                                child: Text(S.of(context).no_data_available,
 
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,),
-                    ),
-                  ),
-
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 70, right: 15),
-                            child: MaterialButton(
-                              color: Colors.lightBlue,
-                              child:  Text(S.of(context).view_demo,style:  TextStyle(color: Colors.white),),
-                              onPressed:() async {
-                                Navigator.of(context).pop(true);
-                              },
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,),
+                              ),
                             ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 5, right: 78),
-                            child: MaterialButton(
-                              color: Colors.lightBlue,
-                              child:  Text(S.of(context).load_data,style:  TextStyle(color: Colors.white),),
-                              onPressed:(){
-                                Navigator.of(context).pop(false);
-                              },
-                            ),
-                          ),
-                        ),
 
-                      ],
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 70, right: 15),
+                                      child: MaterialButton(
+                                        color: Colors.lightBlue,
+                                        child:  Text(S.of(context).view_demo,style:  TextStyle(color: Colors.white),),
+                                        onPressed:() async {
+                                          Navigator.of(context).pop(FuenteData.defaultData);
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 5, right: 78),
+                                      child: MaterialButton(
+                                        color: Colors.lightBlue,
+                                        child:  Text(S.of(context).load_data,style:  TextStyle(color: Colors.white),),
+                                        onPressed:(){
+                                          Navigator.of(context).pop(FuenteData.newData);
+                                        },
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        );
+                  );
 
-      });
-
-
-  return dataDefault ?? false;
+                });
 }
