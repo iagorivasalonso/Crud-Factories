19c6789d14fb97f608740411b49e760b645f0d8a
Iago Rivas <iagorivas@gmail.com>
2026-08-21 13:49:15 +0200

    cambio para ver en movil temporalmente

--- lib/Functions/isNotAndroid.dart
+++ lib/Functions/isNotAndroid.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 bool isNotAndroid() {
-  return !kIsWeb && Platform.isAndroid;
+  return kIsWeb && Platform.isAndroid;  //cambio temporal pra la beta
 }
