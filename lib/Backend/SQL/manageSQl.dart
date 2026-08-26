94c96cdec62134bfe93d7ddb02ce7c180b699da2
Iago Rivas <iagorivas@gmail.com>
2026-08-26 13:41:13 +0200

    Prepara estructura de sesiones de usuario

--- lib/Backend/SQL/manageSQl.dart
+++ lib/Backend/SQL/manageSQl.dart
@@ -70,6 +70,23 @@ class actionsBD {
           'secure BOOLEAN NOT NULL,'
           'password VARCHAR(255) NOT NULL)'
       );
+
+      await conn.query('CREATE TABLE IF NOT EXIST users'
+             '(id INT AUTO_INCREMENT PRIMARY KEY,'
+             'username VARCHAR(100) NOT NULL UNIQUE,'
+             'password_hash VARCHAR(255) NOT NULL,'
+             'role VARCHAR(30) NOT NULL DEFAULT "user",'
+             'active BOOLEAN NOT NULL DEFAULT TRUE,'
+             'create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+      );
+      
+      await conn.query('CREATE TABLE IF NOT EXIST sesions'
+             'id VARCHAR(128) PRIMARY KEY,'
+             'user_id INT NOT NULL,'
+             'created_at TIMESTAMP DEFAULT,'
+             'expires_at TIMESTAMP NOT NULL,'
+             'FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE'
+      );
       return true;
     }catch(e){
       print(e);
